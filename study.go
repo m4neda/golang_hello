13c0e94c4b57e036c79260dd12451e91fcac28e5
m4neda <m4neda@gmail.com>
2020-12-21 15:33:29 +0900

    add check type

--- study.go
+++ study.go
@@ -9,12 +9,17 @@ var (
 	t, f bool    = true, false
 )
 
-func main() {
-
-	fmt.Println(i, f64, s, t, f)
+func foo() {
 	xi := 1
-	xf64 := 1.2
+	var xf64 float32 = 1.2
 	xs := "test"
 	xt, xf := true, false
 	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Printf("%T\n", xi)
+	fmt.Printf("%T\n", xf64)
+}
+
+func main() {
+	fmt.Println(i, f64, s, t, f)
+	foo()
 }
