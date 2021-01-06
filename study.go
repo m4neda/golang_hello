6a523e7ffb8b96fe55ea0d194919abe6a9f1b037
m4neda <m4neda@gmail.com>
2021-01-06 16:11:22 +0900

    learn error handling

--- study.go
+++ study.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"os"
 )
 
-// LoggingSettings Settting log format
-func LoggingSettings(logFile string) {
-	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multilogFile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	log.SetOutput(multilogFile)
-}
-
 func main() {
-	LoggingSettings("test.log")
-
-	// _, err := os.Open("faege")
-	// if err != nil {
-	// 	log.Fatalln("Exit", err)
-	// }
-	// log.Println("logging!")
-	// log.Printf("%T %v", "test", "test")
-
-	// log.Fatalf("%T %v", "test", "test")
-	// log.Fatalln("error")
+	file, err := os.Open("./study.go")
+	if err != nil {
+		log.Fatal("Error!")
+	}
+	defer file.Close()
+	data := make([]byte, 100)
+	// initialize count, overwrite err
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
+	// can not use ':=' because err was already initilalized
+	err = os.Chdir("test")
+	if err != nil {
+		log.Fatalln("Error")
+	}
 }
