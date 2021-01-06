8f9e3167b8c192cb7a438786b2fdb397676bf3c1
m4neda <m4neda@gmail.com>
2021-01-06 10:57:10 +0900

    learn map

--- study.go
+++ study.go
@@ -3,27 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	n := make([]int, 3, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 0, 0)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 0, 0)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["banana"])
+	m["banana"] = 300
+	fmt.Println(m)
+	m["new"] = 500
+	fmt.Println(m)
 
-	a := make([]int, 3)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+	fmt.Println(m["nothing"])
 
-	b := make([]int, 0)
-	var c []int
-	fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
 
-	fmt.Println("Practice")
-	// c = make([]int, 5)
-	c = make([]int, 0, 5)
-	for i := 0; i < 5; i++ {
-		c = append(c, i)
-		fmt.Println(c)
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
+
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.Println(m2)
+
+	// var m3 map[string]int
+	// m3["pc"] = 5000
+	// fmt.Println(m3)
+
+	var s []int
+	if s == nil {
+		fmt.Println("Nil")
 	}
-	fmt.Println(c)
 }
