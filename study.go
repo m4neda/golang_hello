3fd40f09471166f25c27fd4de8edc73361912a07
m4neda <m4neda@gmail.com>
2021-01-06 13:45:02 +0900

    learn variadic function parameters

--- study.go
+++ study.go
@@ -2,31 +2,17 @@ package main
 
 import "fmt"
 
-func incrementGenerator() func() int {
-	x := 0
-	return func() int {
-		x++
-		return x
-	}
-}
-
-func circleArea(pi float64) func(radius float64) float64 {
-	return func(radius float64) float64 {
-		return pi * radius * radius
+func foo(params ...int) {
+	fmt.Println(len(params), params)
+	for _, param := range params {
+		fmt.Println(param)
 	}
 }
 func main() {
-	counter := incrementGenerator()
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
+	foo(10, 20)
+	foo(10, 20, 30)
 
-	c1 := circleArea(3.14)
-	fmt.Println(c1(2))
-	fmt.Println(c1(3))
-
-	c2 := circleArea(3)
-	fmt.Println(c2(2))
-	fmt.Println(c2(3))
+	s := []int{1, 2, 3}
+	fmt.Println(s)
+	foo(s...)
 }
