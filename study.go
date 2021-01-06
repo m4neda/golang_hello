2f641b2b27c846b2f4a0eb09af5fa0305fe19a99
m4neda <m4neda@gmail.com>
2021-01-06 15:09:42 +0900

    learn range

--- study.go
+++ study.go
@@ -11,21 +11,18 @@ func by2(num int) string {
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			fmt.Println("continue")
-			continue
-		}
-		if i > 5 {
-			fmt.Println("break")
-			break
-		}
-		fmt.Println(i)
+	l := []string{"python", "go", "java"}
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
 	}
-	sum := 1
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
+	for i, v := range l {
+		fmt.Println(i, v)
 	}
-	fmt.Println(sum)
+	m := map[string]int{"apple": 100, "banana": 200}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	for k := range m {
+		fmt.Println(k)
+	}
 }
