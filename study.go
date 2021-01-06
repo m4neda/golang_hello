023e984832582e1c770b1e9077f65eb590dfc0e3
m4neda <m4neda@gmail.com>
2021-01-06 16:19:54 +0900

    learn panic

--- study.go
+++ study.go
@@ -1,27 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
+import "fmt"
 
+func thirdPartyConnectDB() {
+	panic("Unable to connect database")
+}
+
+func save() {
+	defer func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+	thirdPartyConnectDB()
+}
 func main() {
-	file, err := os.Open("./study.go")
-	if err != nil {
-		log.Fatal("Error!")
-	}
-	defer file.Close()
-	data := make([]byte, 100)
-	// initialize count, overwrite err
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatalln("Error")
-	}
-	fmt.Println(count, string(data))
-	// can not use ':=' because err was already initilalized
-	err = os.Chdir("test")
-	if err != nil {
-		log.Fatalln("Error")
-	}
+	save()
+	fmt.Println("OK?")
 }
