cb1893456c6d258c881b0d0827b84f82e64b9106
m4neda <m4neda@gmail.com>
2021-01-08 16:21:57 +0900

    learn channel

--- study.go
+++ study.go
@@ -1,31 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func goroutine(s string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i < 5; i++ {
-		// time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
-}
-
-func normal(s string) {
-	for i := 0; i < 5; i++ {
-		// time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
-	}
+	c <- sum
 }
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1) // there is one parallel function
-	go goroutine("World", &wg)
-	normal("Hello")
-	time.Sleep(2000 * time.Millisecond)
-	wg.Wait()
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go goroutine1(s, c)
+	go goroutine1(s, c)
+	x := <-c // will wait until goroutine finished
+	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
 }
