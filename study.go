76bddab1bf41c6ed28da0ee2155e40766ac56002
m4neda <m4neda@gmail.com>
2021-01-08 12:08:10 +0900

    learn type assertion

--- study.go
+++ study.go
@@ -2,32 +2,28 @@ package main
 
 import "fmt"
 
-type Human interface {
-	Say() string
-}
-type Person struct {
-	Name string
-}
+func do(i interface{}) {
+	// ii := i.(int)
+	// ii *= 2
+	// fmt.Println(ii)
 
-type Dog struct {
-	Name string
-}
-
-func (p *Person) Say() string {
-	p.Name = "Mr." + p.Name
-	return p.Name
-}
-func DriveCar(human Human) {
-	if human.Say() == "Mr.Mike" {
-		fmt.Println("Run")
-	} else {
-		fmt.Println("Get out")
+	// type assertion
+	switch v := i.(type) {
+	case int:
+		fmt.Println(v * 2)
+	case string:
+		fmt.Println(v + "!")
+	default:
+		fmt.Printf("I don't know %T\n", v)
 	}
-
 }
+
 func main() {
-	var mike Human = &Person{"Mike"}
-	DriveCar(mike)
-	// var dog Dog = Dog{"dog"}
-	// DriveCar(dog)
+	do(10)
+	do("Mike")
+	do(true)
+	// type conversion
+	var i int = 10
+	ii := float64(i)
+	fmt.Println(i, ii)
 }
