48dbfcea235806378639dca67f8213692990451b
m4neda <m4neda@gmail.com>
2021-01-08 14:31:15 +0900

    learn custom error

--- study.go
+++ study.go
@@ -1,29 +1,23 @@
 package main
 
-import "fmt"
-
-func do(i interface{}) {
-	// ii := i.(int)
-	// ii *= 2
-	// fmt.Println(ii)
-
-	// type assertion
-	switch v := i.(type) {
-	case int:
-		fmt.Println(v * 2)
-	case string:
-		fmt.Println(v + "!")
-	default:
-		fmt.Printf("I don't know %T\n", v)
-	}
-}
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 func main() {
-	do(10)
-	do("Mike")
-	do(true)
-	// type conversion
-	var i int = 10
-	ii := float64(i)
-	fmt.Println(i, ii)
+	r := strings.NewReader("Hello World!")
+
+	b := make([]byte, 8)
+	// e := errors.New("EOF")
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		// if err == e {
+		if err == io.EOF {
+			break
+		}
+	}
 }
