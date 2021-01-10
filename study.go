26dbdd772d6927533bd96706ef8fc11631d8f631
m4neda <m4neda@gmail.com>
2021-01-10 10:19:48 +0900

    learn sync.mutex

--- study.go
+++ study.go
@@ -2,35 +2,45 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func goroutine1(ch chan string) {
-	for {
-		ch <- "packet from 1"
-		time.Sleep(1 * time.Second)
-	}
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
 }
 
-func goroutine2(ch chan string) {
-	for {
-		ch <- "packet from 2"
-		time.Sleep(1 * time.Second)
-	}
+func (c *Counter) Inc(key string) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
 }
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
-	go goroutine1(c1)
-	go goroutine2(c2)
+	// c := make(map[string]int)
+	c := Counter{v: make(map[string]int)}
 
-	for {
-		select {
-		case msg1 := <-c1:
-			fmt.Println(msg1)
-		case msg2 := <-c2:
-			fmt.Println(msg2)
+	go func() {
+		for i := 0; i < 10; i++ {
+			// c["key"] += 1
+			c.Inc("key")
 		}
-	}
+	}()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			// c["key"] += 1
+			c.Inc("key")
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	// fmt.Println(c, c["key"])
+	fmt.Println(c, c.Value("key"))
 }
